318c1cfce43778889b9926e3766a53949de2075c
Pere.Grima <pere.grima@upc.edu>
2024-05-19 10:59:30 +0200

    Avanzando

--- regresionSimple/200_ArbolManoModelos.R
+++ regresionSimple/200_ArbolManoModelos.R
@@ -19,11 +19,16 @@ axis(side=1, padj=-2, tck=-0.02, at=seq(6, 22, 2), cex.axis = t)
 axis(side=2, hadj=0.1, tck=-0.02, at=seq(10, 90, 10), cex.axis = t, las = 1)
 axis(side=2, hadj=-0.7, tck=-0.02, at = 0, cex.axis = t, las = 1)
 
-mtext('Diámetro (pies)', side=1, line=1, cex=t+0.1, las=1, family = "serif")
-mtext(expression("Volumen de madera (pies" ^ "3", "                                          )"), 
+mtext('X: Diámetro (pies)', side=1, line=1, cex=t+0.1, las=1, family = "serif")
+mtext(expression("Y: Volumen de madera (pies" ^ "3", "                                                )"), 
       side=2, line=1.5, cex=t+0.1, las=0, family = "serif")
 text(14, 96, TeX("$ Vol. = -36.94 + 5.066 \\cdot diam$"), cex = t+0.15, xpd = NA)
-text(18.5, 10, TeX("$ italic(s_R) = 4.25$"), cex = t+0.15, xpd = NA)
+
+text(8.4, 82, TeX("$ italic(n) = 31$"), cex = t+0.15, xpd = NA)
+text(9, 74, TeX("$ italic(s_R) = 4.25$"), cex = t+0.15, xpd = NA)
+text(9, 66, TeX("$ italic(\\bar{x}) = 13.25$"), cex = t+0.15, xpd = NA)
+text(8.6, 58, TeX("$ italic(s_X) = 3.2$"), cex = t+0.15, xpd = NA)
+text(8.6, 50, TeX("$ italic(\\bar{y}) = 30.2$"), cex = t+0.15, xpd = NA)
 
 abline(lm(yArbol~xArbol), col = "blue")
 #####################
@@ -36,10 +41,15 @@ axis(side=1, padj=-2, tck=-0.02, at=seq(4, 14, 2), cex.axis = t)
 axis(side=2, hadj=-0, tck=-0.02, at=seq(10, 90, 10), cex.axis = t, las = 1)
 axis(side=2, hadj=0.35, tck=-0.02, at=100, cex.axis = t, las = 1)
 
-mtext('Longitud línea mano (cm)', side=1, line=1, cex=t+0.1, las=1, family = "serif")
-mtext('Edad al morir', side=2, line=1.7, cex=t+0.1, las=0, family = "serif")
+mtext('X: Longitud línea mano (cm)', side=1, line=1, cex=t+0.1, las=1, family = "serif")
+mtext('Y: Edad al morir', side=2, line=1.7, cex=t+0.1, las=0, family = "serif")
 text(9, 106, TeX("$ Edad = 79.23 - 1.367 \\cdot long$"), cex = t+0.15, xpd = NA)
-text(6.5, 20, TeX("$ italic(s_R) = 14.15$"), cex = t+0.15, xpd = NA)
+
+text(5.3, 50, TeX("$ italic(n) = 50$"), cex = t+0.15, xpd = NA)
+text(5.9, 42, TeX("$ italic(s_R) = 14.15$"), cex = t+0.15, xpd = NA)
+text(5.4, 34, TeX("$ italic(\\bar{x}) = 9.2$"), cex = t+0.15, xpd = NA)
+text(5.7, 26, TeX("$ italic(s_X) = 1.27$"), cex = t+0.15, xpd = NA)
+text(5.6, 18, TeX("$ italic(\\bar{y}) = 66.7$"), cex = t+0.15, xpd = NA)
 
 abline(lm(yMano~xMano), col = "blue")
 
